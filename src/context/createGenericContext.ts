23ec3668d5c568359697b2ee40d7d3349ab0698b
irfan zahir <irfan.zahirharon@gmail.com>
2023-08-08 09:15:52 +0000

    added user context

---
+++ src/context/createGenericContext.ts
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const createGenericContext = <T extends unknown>() => {
+    // Create a context with a generic parameter or undefined
+    const genericContext = React.createContext<T | undefined>(undefined);
+
+    // Check if the value provided to the context is defined or throw an error
+    const useGenericContext = () => {
+    };
+
+    return [useGenericContext, genericContext.Provider] as const;
+};
